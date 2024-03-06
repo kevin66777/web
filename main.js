5a178bf042c0e74aabd3f6b29a7a8034d6f2578f
kevin66777 <146474952+kevin66777@users.noreply.github.com>
2024-03-06 12:21:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,23 @@ let typewriter = new Typewriter(app, {
 });
  
 typewriter
-  .pauseFor(1800)
+  .pauseFor(1200)
   .typeString('Hola, Soy un Desarrollador Web Junior dispuesto a grandes retos..')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+ 
+  let app2 = document.getElementById('typewriter2');
+ 
+  let typewriter2 = new Typewriter(app2, {
+    loop: true,
+    delay: 75,
+  });
+   
+
+typewriter2
+  .pauseFor(1200)
+  .typeString('He trabajado en algunos proyectos tales como...')
   .pauseFor(200)
   .deleteChars(10)
   .start();
